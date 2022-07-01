05e91c096a4dbd5e496e0056d985a9db6f3d93dd
Lopo <69903511+TTX-Lopo@users.noreply.github.com>
2022-07-01 16:38:42 +0200

    Ragdoll Plugin for FiveM

---
+++ README.md
@@ -0,0 +1,6 @@
+# BroClub ragdoll plugin 
+This resource is free to use 
+
+Press 'g' to ragdoll and stop it with press it again
+Change it in settings under key binds --> FiveM --> Ragdoll
+
---
+++ client.lua
@@ -0,0 +1,51 @@
+local ragdoll = false
+
+Citizen.CreateThread(function() --Create a Thread
+    while true do
+        Wait(0)
+        if  ragdoll then
+            SetPedToRagdoll(PlayerPedId(),1000,1000,0,true,true,false)--1000ms
+        end
+    end
+end)
+
+RegisterCommand('ragdoll', function() --Register our ragdoll command
+    if not IsPedInAnyVehicle(PlayerPedId(), false) then
+        ragdoll = not ragdoll
+    end
+end,false)
+
+RegisterKeyMapping('ragdoll', 'Ragdoll', 'keyboard', 'g') -- add our key mapping
+
+
+
+--[[
+
+--Version of ragdoll with press and release
+
+local ragdoll = false
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        if  ragdoll then
+            SetPedToRagdoll(PlayerPedId(),-1,-1,0,true,true,false)
+        end
+    end
+end)
+
+RegisterCommand('+ragdoll', function()
+        if not IsPedInAnyVehicle(PlayerPedId(), false) then
+            ragdoll = true
+        end
+end,false)
+
+RegisterCommand('-ragdoll',function()
+        if not IsPedInAnyVehicle(PlayerPedId(), false) then
+            ragdoll = true
+        end
+end,false)
+
+RegisterKeyMapping('+ragdoll', 'Ragdoll', 'keyboard', 'g')
+
+]]--
